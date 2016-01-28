114448a8af1d99dbc0df43a2fba60c4620a8e4de
galladoroman <gallardoroman@gmail.com>
2016-01-28 20:34:16 +0100

    cambio 280116

--- app/scripts/controllers/simon.js
+++ app/scripts/controllers/simon.js
@@ -8,30 +8,65 @@
  * Controller of the simonApp
  */
 angular.module('simonApp')
-  .controller('SimonCtrl', function () {
-    $(document).ready(function(){
-			$(".green").click(function(){
-				$("h1").text("verde")
-			})
-	})
+  .controller('SimonCtrl', function ($scope) {
 
-	 $(document).ready(function(){
-			$(".red").click(function(){
-				$("h1").text("rojo")
-			})
-	})
+ 		$scope.colores = [];   
+  		$scope.titulo = 'Simón';
+  		$scope.acumulador = 0;
+  		$scope.acumuladorverde = 0;
+  		$scope.acumuladorrojo = 0;
+  		$scope.acumuladorazul = 0;
+  		$scope.acumuladoramarillo = 0;
 
 
-	 $(document).ready(function(){
-			$(".blue").click(function(){
-				$("h1").text("azul")
-			})
-	})
+    	$scope.play=function(color){
+
+    		switch (color) {
+    case ('verde'):
+        $scope.acumuladorverde++;
+        break;
+    case ('rojo'):
+        $scope.acumuladorrojo++;
+        break;
+    case ('azul'):
+        $scope.acumuladorazul++;
+        break;
+    /*case ('amarillo'):
+        $scope.acumuladoramarillo++;
+        break;*/
+    default:
+        alert('La has cagado');
+    }
+    		/*if(color=='verde'){
+    			
+    			$scope.acumuladorverde++;
+    		} 
+    		else if(color=='rojo') {
+    		
+    			$scope.acumuladorrojo++;
+
+			} else if(color=='azul') {
+    		
+				$scope.acumuladorazul++;
+
+			} else if(color=='amarillo') {
+
+    			$scope.acumuladoramarillo++;
+			}*/
+		
 
 
-	 $(document).ready(function(){
-			$(".yellow").click(function(){
-				$("h1").text("amarillo")
-			})
-	})
+
+    		$scope.colores.push(color);
+    	}
+
+    	$scope.borrar=function(){
+    		$scope.colores = [];
+    		$scope.acumulador = 0;
+    		$scope.acumuladorojo = 0;
+    		$scope.acumuladorazul = 0;
+    		$scope.acumuladorverde = 0;
+    		$scope.acumuladoramarillo = 0;
+
+    	}
   });
